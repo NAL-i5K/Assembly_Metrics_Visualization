836557383855f401eeb42b6623f486a58c9d61f2
Yi Hsiao <hsiaoyi0504@gmail.com>
2017-09-04 20:17:15 +0800

    remove statistics of split contigs

--- js/assembly-table.js
+++ js/assembly-table.js
@@ -1,40 +1,40 @@
 function plotAssemblyTable(json) {
-    var splitContigs = json.hasOwnProperty('Median split contig size');
+    var hasContigs = json.hasOwnProperty('Number of contigs');
     var parent = d3.select('#assembly-table');
     var table = parent.append('table');
     var thead = table.append('thead');
     var tbody = table.append('tbody');
-    if(splitContigs) {
-      var columnNames = ['Property','Scaffolds','Split Contigs'];
+    if(!hasContigs) {
+      var columnNames = ['Property','Scaffolds'];
       var rowMap = {
-        'Total number of sequences': ['Number of scaffolds','Number of split contigs'],
-        'Total size of sequences': ['Total size of scaffolds','Total size of split contigs'],
-        'Longest sequence': ['Longest scaffold','Longest split contig'],
-        'Shortest sequqnce': ['Shortest scaffold','Shortest split contig'],
-        'Number of sequences (>= 1k nt)':['Number of scaffolds (>= 1k nt)','Number of split contigs (>= 1k nt)'],
-        'Number of sequences (>= 10k nt)':['Number of scaffolds (>= 10k nt)','Number of split contigs (>= 10k nt)'],
-        'Number of sequences (>= 100k nt)':['Number of scaffolds (>= 100k nt)','Number of split contigs (>= 100k nt)'],
-        'Number of sequences (>= 1M nt)':['Number of scaffolds (>= 1M nt)','Number of split contigs (>= 1M nt)'],
-        'Number of sequences (>= 10M nt)':['Number of scaffolds (>= 1M nt)','Number of split contigs (>= 10M nt)'],
-        'Total length of sequences (>= 1k nt)':['Total length of scaffolds (>= 1k nt)','Total length of split contigs (>= 1k nt)'],
-        'Total length of sequences (>= 10k nt)':['Total length of scaffolds (>= 10k nt)','Total length of split contigs (>= 10k nt)'],
-        'Total length of sequences (>= 100k nt)':['Total length of scaffolds (>= 100k nt)','Total length of split contigs (>= 100k nt)'],
-        'Total length of sequences (>= 1M nt)':['Total length of scaffolds (>= 1M nt)','Total length of split contigs (>= 1M nt)'],
-        'Total length of sequences (>= 10M nt)':['Total length of scaffolds (>= 10M nt)','Total length of split contigs (>= 10M nt)'],
-        'Mean sequence size':['Mean scaffold size','Mean split contig size'],
-        'Median sequeuce size':['Median scaffold size','Mean split contig size'],
-        ' A (%)':['Scaffold A (%)','Split contig A (%)'],
-        ' C (%)':['Scaffold C (%)','Split contig C (%)'],
-        ' G (%)':['Scaffold G (%)','Split contig G (%)'],
-        ' T (%)':['Scaffold T (%)','Split contig T (%)'],
-        ' N (%)':['Scaffold N (%)','Split contig N (%)'],
-        'Non-ACGTN (%)':['Scaffold non-ACGTN (%)','Split contig non-ACGTN (%)'],
-        'GC content of sequences (%)':['GC content of scaffolds (%)','GC content of split contigs (%)'],
-        'N50 of sequences':['N50 of scaffolds','N50 of split contigs'],
-        'N75 of sequences':['N75 of scaffolds','N75 of split contigs'],
-        'L50 of sequences':['L50 of scaffolds','L50 of split contigs'],
-        'L75 of sequences':['L75 of scaffolds','L75 of split contigs'],
-        'Number of N per 100k nt of sequences':['Number of N per 100k nt of scaffolds','Number of N per 100k nt of split contigs'],
+        'Total number of sequences': ['Number of scaffolds'],
+        'Total size of sequences': ['Total size of scaffolds'],
+        'Longest sequence': ['Longest scaffold'],
+        'Shortest sequqnce': ['Shortest scaffold'],
+        'Number of sequences (>= 1k nt)':['Number of scaffolds (>= 1k nt)'],
+        'Number of sequences (>= 10k nt)':['Number of scaffolds (>= 10k nt)'],
+        'Number of sequences (>= 100k nt)':['Number of scaffolds (>= 100k nt)'],
+        'Number of sequences (>= 1M nt)':['Number of scaffolds (>= 1M nt)'],
+        'Number of sequences (>= 10M nt)':['Number of scaffolds (>= 1M nt)'],
+        'Total length of sequences (>= 1k nt)':['Total length of scaffolds (>= 1k nt)'],
+        'Total length of sequences (>= 10k nt)':['Total length of scaffolds (>= 10k nt)'],
+        'Total length of sequences (>= 100k nt)':['Total length of scaffolds (>= 100k nt)'],
+        'Total length of sequences (>= 1M nt)':['Total length of scaffolds (>= 1M nt)'],
+        'Total length of sequences (>= 10M nt)':['Total length of scaffolds (>= 10M nt)'],
+        'Mean sequence size':['Mean scaffold size'],
+        'Median sequeuce size':['Median scaffold size'],
+        ' A (%)':['Scaffold A (%)'],
+        ' C (%)':['Scaffold C (%)'],
+        ' G (%)':['Scaffold G (%)'],
+        ' T (%)':['Scaffold T (%)'],
+        ' N (%)':['Scaffold N (%)'],
+        'Non-ACGTN (%)':['Scaffold non-ACGTN (%)'],
+        'GC content of sequences (%)':['GC content of scaffolds (%)'],
+        'N50 of sequences':['N50 of scaffolds'],
+        'N75 of sequences':['N75 of scaffolds'],
+        'L50 of sequences':['L50 of scaffolds'],
+        'L75 of sequences':['L75 of scaffolds'],
+        'Number of N per 100k nt of sequences':['Number of N per 100k nt of scaffolds'],
       };
     } else {
       var columnNames = ['Property','Scaffolds','Contigs'];
